6bae7550e560191f742275fc0ab2be5495b2cab7
Nicholas Manos <manos.nick@gmail.com>
2014-10-15 20:04:50 -0700

    pulling out caching for heroku v2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
-#    @posts = Post.paginate(:page => params[:page], :per_page => 10).includes(:comments).all
+#    @posts = Post.all
+    @posts = Post.paginate(:page => params[:page], :per_page => 10).includes(:comments).all
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @posts }
